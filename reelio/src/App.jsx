383b5164ad4570b457aa8a3157caeef50e99259b
Kundan Goyary <kgoyary824@gmail.com>
2026-09-10 11:14:46 +0530

    Integrate ToastProvider and ToastBridge in App

--- reelio/src/App.jsx
+++ reelio/src/App.jsx
@@ -1,25 +1,41 @@
-import { Routes, Route } from 'react-router-dom';
-import MainLayout from './layouts/MainLayout';
-import Home from './pages/Home';
-import Movies from './pages/Movies';
-import TV from './pages/TV';
-import Timeline from './pages/Timeline';
-import MovieDetails from './pages/MovieDetails';
-import TVDetails from './pages/TVDetails';
-import SearchResults from './pages/SearchResults';
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import TV from "./pages/TV";
+import Timeline from "./pages/Timeline";
+import MovieDetails from "./pages/MovieDetails";
+import TVDetails from "./pages/TVDetails";
+import SearchResults from "./pages/SearchResults";
+import { ToastProvider, useToast } from "./components/Toast";
+import { registerToastHandlers } from "./services/tmdb";
+
+// Bridges Toast context to the plain-JS tmdb.js module, so tmdbGet()
+// can trigger toasts without needing React context itself.
+function ToastBridge() {
+  const { showToast, dismissToast } = useToast();
+  useEffect(() => {
+    registerToastHandlers({ showToast, dismissToast });
+  }, [showToast, dismissToast]);
+  return null;
+}
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/tv" element={<TV />} />
-        <Route path="/timeline" element={<Timeline />} />
-        <Route path="/movie/:id" element={<MovieDetails />} />
-        <Route path="/tv/:id" element={<TVDetails />} />
-        <Route path="/search" element={<SearchResults />} />
-      </Route>
-    </Routes>
+    <ToastProvider>
+      <ToastBridge />
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/tv" element={<TV />} />
+          <Route path="/timeline" element={<Timeline />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="/tv/:id" element={<TVDetails />} />
+          <Route path="/search" element={<SearchResults />} />
+        </Route>
+      </Routes>
+    </ToastProvider>
   );
 }
